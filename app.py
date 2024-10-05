e46e8756880f875473b75b1b58ec67ee5d2475f1
Mtiwari27 <126952539+Mtiwari27@users.noreply.github.com>
2024-10-05 15:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_gemini_response(input,image):
 
 
 st.set_page_config(
-    page_title="ImageInterpreter",
+    page_title="Image-Interpreter",
     page_icon="App Icon.png")
 st.header("Image Interpreter")
 st.markdown("""
